7d3c670ca8ca6ec3d2ff37ea027298b5556b9c33
wdd119 <1240172019@qq.com>
2017-10-25 23:10:25 +0800

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MdSidenavModule } from '@angular/material'
+import { CoreModule } from './core/core.module'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    CoreModule,
+    MdSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
