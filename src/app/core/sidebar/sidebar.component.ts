a8afabe54120ec60e4b9fc46ef9441b506987f97
wdd119 <1240172019@qq.com>
2017-11-01 23:49:13 +0800

    update

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { getDate } from 'date-fns'
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  today = 'day'
+  
   constructor() { }
 
   ngOnInit() {
+      this.today = `day${getDate(new Date())}`
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,7 +5,8 @@ import {
   MdIconModule, 
   MdCardModule, 
   MdButtonModule,
-  MdInputModule } from '@angular/material'
+  MdInputModule,
+  MdListModule } from '@angular/material'
 
 @NgModule({
   imports: [
@@ -14,7 +15,8 @@ import {
     MdIconModule,
     MdButtonModule,
     MdCardModule,
-    MdInputModule
+    MdInputModule,
+    MdListModule
   ],
   exports:[
     CommonModule,
@@ -22,7 +24,8 @@ import {
     MdIconModule,
     MdButtonModule,
     MdCardModule,
-    MdInputModule
+    MdInputModule,
+    MdListModule
   ],
   declarations: []
 })
