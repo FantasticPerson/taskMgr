b894310c6a59b288bfd136de6525473b1938ed4b
wdd119 <1240172019@qq.com>
2017-12-14 22:13:04 +0800

    update

--- src/app/shared/age-input/age-input.component.ts
+++ src/app/shared/age-input/age-input.component.ts
@@ -1,6 +1,29 @@
 import { Component, OnInit, forwardRef } from '@angular/core';
 import { ControlValueAccessor,NG_VALUE_ACCESSOR,NG_VALIDATORS,FormControl,FormBuilder,FormGroup } from '@angular/forms'
 import { Observable } from 'rxjs/Observable';
+import {
+  subDays,
+  subMonths,
+  subYears,
+  differenceInDays,
+  differenceInYears,
+  isBefore,
+  parse,
+  differenceInMonths,
+  format
+} from 'date-fns'
+// import { format } from 'path';
+
+export enum AgeUnit{
+  Year=0,
+  Month,
+  day
+}
+
+export interface Age{
+  age:number;
+  unit:AgeUnit;
+}
 
 @Component({
   selector: 'app-age-input',
@@ -14,32 +37,83 @@ export class AgeInputComponent implements ControlValueAccessor{
 
   ngOnInit() {
     this.form = this.fb.group({
-      birthday:[],
+      birthday:['',this.validateDate],
       age:this.fb.group({
         ageNum:[],
         ageUnit:[]
-      })
+      },{validator:this.validateAge('ageNum','ageUnit')})
     })
 
     const birthday = this.form.get('birthday')
     const ageNum = this.form.get('ageNum')
     const ageUnit = this.form.get('ageUnit')
 
-    const birthday$ = birthday.valueChanges.map(d=>{
-      return {data:d,from:'birthday'}
-    })
+    const birthday$ = birthday.valueChanges
+      .map(d=>{
+        return {data:d,from:'birthday'}
+      })
+      .filter(_=>birthday.valid)
+      .debounceTime(300)
+      .distinctUntilChanged()
     const ageNum$ = ageNum.valueChanges
+      .startWith(ageNum.value)
+      .debounceTime(300)
+      .distinctUntilChanged()
     const ageUnit$ = ageUnit.valueChanges
-    const age$ = Observable.combineLatest(ageNum$,ageUnit$,(_n,_u)=>{
-      return this.toDate({age:_n,unit:_u})
-    })
-    .map(d=>{
-      return {date:d,from:'age'}
-    })
+      .startWith(ageUnit.value)
+      .debounceTime(300)
+      .distinctUntilChanged()
+    const age$ = Observable
+      .combineLatest(ageNum$,ageUnit$,(_n,_u)=>{
+        return this.toDate({age:_n,unit:_u})
+      })
+      .map(d=>{
+        return {date:d,from:'age'}
+      })
+      .filter(_=>this.form.get('age').valid)
 
-    const merged$ = Observable.merge(birthday$,age$);
-
+    const merged$ = Observable
+      .merge(birthday$,age$)
+      .filter(_=>this.form.valid);
+    merged$.subscribe(d=>{
+      const age = this.toAge(d.date);
+      if(d.from !== 'birthday'){
+        if(age.age !== ageNum.value){
+          ageNum.patchValue(age.age,{emitEvent:false})
+        }
+        if(age.unit !== ageUnit.value){
+          ageUnit.patchValue(age.unit,{emitEvent:false})
+        }
+        this.propagateChange(d.date)
+      } else {
+        const ageToCompare = this.toAge(birthday.value)
+        if(age.age !== ageToCompare.age || age.unit !== ageToCompare.unit){
+          birthday.patchValue(d.date,{emitEvent:false})
+          this.propagateChange(d.date)
+        }
+      }
+    })
     
+  }
+
+  validate(c:FormControl):{[key:string]:any}{
+    
+  }
+
+  validateDate(c:FormControl):{[key:string]:any}{
+    return this.selected ? null : {
+      imageListInvalid:{
+        valid:false
+      }
+    }
+  }
+
+  validateAge(ageNumKey:string,ageUnitKey:string):{[key:string]:any}{
+    return this.selected ? null : {
+      imageListInvalid:{
+        valid:false
+      }
+    }
   }
 
   writeValue(obj: any): void{
@@ -52,5 +126,37 @@ export class AgeInputComponent implements ControlValueAccessor{
 
   registerOnTouched(fn: any): void{
 
+  }
+
+  toAge(dateStr:string):Age{
+    const date = parse(dateStr)
+    const now = Date.now()
+    return isBefore(subDays(now,90),date) ? 
+      { age:differenceInDays(now,date),unit:AgeUnit.day} :
+        isBefore(subMonths(now,24),date) ? 
+          {age:differenceInMonths(now,date),unit:AgeUnit.Month} :
+            {
+              age:differenceInYears(now,date),
+              unit:AgeUnit.Year
+            };
+  }
+
+  toDate(age:Age):string{
+    const now = Date.now();
+    const dateFormat = "YYYY-MM-DD"
+    switch(age.unit){
+      case AgeUnit.Year:{
+        return format(subYears(now,age.age),dateFormat)
+      }
+      case AgeUnit.Year:{
+        return format(subMonths(now,age.age),dateFormat)
+      }
+      case AgeUnit.Year:{
+        return format(subDays(now,age.age),dateFormat)        
+      }
+      default:{
+        return null;
+      }
+    }
   }
 }
